bf6c0eb23e37aa531870fde9b07bed769e2daab9
Aleksandr Svetlov <lemmingval@gmail.com>
2022-11-12 12:37:59 +0400

    add basename

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
     </BrowserRouter>
 
